4154325f1494af7c30f193ac0cdcd2bceaf031bc
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-01-17 19:48:17 -0600

    added planets, IK in progress

---
+++ RenegadeHomeTour/Assets/planetoid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planetoid : MonoBehaviour
+{
+    public int numMoons;
+    public float planetScale;
+    public float atmoScale;
+    public float rotSpeed;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(rotSpeed,0);
+        rb.angularDrag = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
