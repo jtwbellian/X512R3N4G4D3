6fa87c5d13c15bb907716fbd7728fe2ef068d457
jtwbellian <32314548+jtwbellian@users.noreply.github.com>
2019-01-24 13:34:17 -0600

    added atmosphere shader

--- RenegadeHomeTour/Assets/IKPlayerController.cs
+++ RenegadeHomeTour/Assets/IKPlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class IKPlayerController : MonoBehaviour
 {
+    public Transform target;
+    public float height = 1.5f;
+    private float turningRate = 0.1f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position != target.position)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y - height, target.position.z);
+        }
+        // Turn towards our target rotation.
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, turningRate * Time.deltaTime);
     }
 }
