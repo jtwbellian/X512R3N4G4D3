b81b0a969d38d17b867192b03869dd45bd92c6e2
BinariDan <dabellian@gmail.com>
2019-02-11 11:43:26 -0600

    attempted to fix door code, no dice

--- RenegadeHomeTour/Assets/_Design/Door/Automation/SlidingDoors.cs
+++ RenegadeHomeTour/Assets/_Design/Door/Automation/SlidingDoors.cs
@@ -3,36 +3,51 @@
 public class SlidingDoors : MonoBehaviour
 {
 	
-	public GameObject DoorTrigger;
 	public GameObject ShortDoor;
 	public GameObject LongDoor;
 
-	Animator ShortAnim;
-	Animator LongAnim;
+	private Animator ShortAnim;
+	private Animator LongAnim;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       	ShortAnim = ShortDoor.GetComponent <Animator > ();
-	LongAnim = LongDoor.GetComponent <Animator > ();
+       	ShortAnim = ShortDoor.GetComponent <Animator> ();
+	    LongAnim = LongDoor.GetComponent <Animator> ();
+        //ShortAnim.updateMode.UnscaledTime;
+        //LongAnim.updateMode.UnscaledTime;
+        Debug.Log((LongAnim == null) ? "Long anim is NULL" : "We good");
+        Debug.Log((ShortAnim == null) ? "Short anim is NULL" : "We good");
     }
 
 	void OnTriggerEnter(Collider coll){
-		if(coll.gameObject.tag == "DoorAble"){
-		SlideDoors (true);
-	}
-	}
+
+        Debug.Log("Trigger entered");
+        //if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.CompareTag("Player"))
+        {
+           // var state = true;
+            SlideDoors(state);
+            LongAnim.Play("Long_Open", 0, 1f);
+            ShortAnim.Play("Short_Open", 0, 1f);
+        }
+    }
 
 	void OnTriggerExit(Collider coll){
-		if(coll.gameObject.tag == "DoorAble"){
-		SlideDoors (false);
 
-	}
+		if (coll.gameObject.CompareTag("Player"))
+        {
+           // var state = false;
+            //SlideDoors(state);
+	    }
 	}
 
-	void SlideDoors(bool state){
-		ShortAnim.SetBool ("Slide", state);
-		LongAnim.SetBool ("Slide", state);
-	}
+//	void SlideDoors(bool state){
+
+//        Debug.Log("ShortAnim = " + ShortAnim.ToString());
+
+		//ShortAnim.SetBool("Slide", state);
+		//LongAnim.SetBool("Slide", state);
+//	}
 }
