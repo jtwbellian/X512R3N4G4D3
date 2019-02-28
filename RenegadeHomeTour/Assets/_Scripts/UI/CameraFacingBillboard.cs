d20b9c4452bcf8a4cdc4c8d51b08ce3850dfae85
BinariDan <dabellian@gmail.com>
2019-02-28 13:15:47 -0600

    added sprite animations, added reflection probe

--- RenegadeHomeTour/Assets/_Scripts/UI/CameraFacingBillboard.cs
+++ RenegadeHomeTour/Assets/_Scripts/UI/CameraFacingBillboard.cs
@@ -9,7 +9,7 @@ public class CameraFacingBillboard : MonoBehaviour
     //Orient the camera after all movement is completed this frame to avoid jittering
     void LateUpdate()
     {
-        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+        transform.LookAt(transform.position + m_Camera.transform.rotation * -Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
     }
 }
